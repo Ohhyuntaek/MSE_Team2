bf83a68e1101c43290503e0c0f5a0afb9fc13ee7
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2024-04-18 23:04:18 +0900

    Implement_SampleScene
    Add a log-in scene and Implement sample scene(Tiles, Background and Animals)

---
+++ mse_team2/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Networking;
+using UnityEditor.PackageManager.Requests;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    private static string BasicURL = "http://localhost:9999/farmwars";
+    private string SignupURL = BasicURL + "/signup";
+    private string LoginURL = BasicURL + "/login";
+
+    [SerializeField]
+    public TMP_InputField IDInput;
+    public TMP_InputField NicknameInput;
+    public TMP_InputField PasswordInput;
+
+    public SignupSceneManager SignupSceneManager;
+
+
+    public void SignupPlayer()
+    {
+        StartCoroutine(SignupRequest());
+    }
+
+    public void LoginPlayer()
+    {
+        StartCoroutine(LoginRequest());
+    }
+
+    IEnumerator SignupRequest()
+    {
+
+        string json = getPlayerFromFields();
+        
+        UnityWebRequest request = UnityWebRequest.Post(SignupURL, json, "application/json");
+
+        yield return request.SendWebRequest();
+
+        switch (request.result)
+        {
+            case UnityWebRequest.Result.ConnectionError:
+            case UnityWebRequest.Result.DataProcessingError:
+                Debug.Log("Error: " + request.error);
+                break;
+            case UnityWebRequest.Result.ProtocolError:
+                Debug.Log("HTTP Error: " + request.error);
+                break;
+            case UnityWebRequest.Result.Success:
+                Debug.Log("Request success");
+                SignupSceneManager.ShowSignupResult(request.downloadHandler.text);
+                
+                break;
+        }
+    }
+
+    IEnumerator LoginRequest()
+    {
+        string json = getLoginInfoFromFields();
+
+        UnityWebRequest request = UnityWebRequest.Post(LoginURL, json, "application/json");
+
+        yield return request.SendWebRequest();
+
+        switch (request.result)
+        {
+            case UnityWebRequest.Result.ConnectionError:
+            case UnityWebRequest.Result.DataProcessingError:
+                Debug.Log("Error: " + request.error);
+                break;
+            case UnityWebRequest.Result.ProtocolError:
+                Debug.Log("HTTP Error: " + request.error);
+                break;
+            case UnityWebRequest.Result.Success:
+                Debug.Log("Request success");
+                Debug.Log(request.downloadHandler.text);
+                break;
+        }
+    }
+    
+    private string getPlayerFromFields()
+    {
+        Player p = new Player();
+        p.SetID(IDInput.text);
+        p.SetNickname(NicknameInput.text);
+        p.SetPassword(PasswordInput.text);
+        
+        return JsonUtility.ToJson(p);
+    }
+
+    private string getLoginInfoFromFields() {
+
+        LoginInfo loginfo = new LoginInfo();
+        loginfo.SetID(IDInput.text);
+        loginfo.SetPassword(PasswordInput.text);
+
+        return JsonUtility.ToJson(loginfo);
+    }
+
+    
+}
